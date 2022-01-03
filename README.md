5344a98191b832c33f74d6a7ffe6233fc4b98fb8
Hao Wu <echowuhao@gmail.com>
2022-01-03 12:35:02 +0800

    add tests

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## How to run flask app
+
+```sh
+export FLASK_APP=account
+export FLASK_ENV=development
+# init db
+# flask shell 
+# db.create_all()
+
+flask run
+```
--- account.py
+++ account.py
@@ -8,6 +8,7 @@ Please use your own GitHub account to upload the code and paste the repository U
 """
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
@@ -25,6 +26,39 @@ class User(db.Model):
         return "<User %r>" % self.username
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def init_db():
+    db.create_all()
+
+
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    @app.route("/user/<username>", methods=['GET'])
+    def get_user_by_username(username):
+        return f"<p>{username}</p>"
+
+    return app
